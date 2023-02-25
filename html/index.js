b6ce8d5129c296bd951ae4dd8bf505ed471faff0
Geraldo Ignacio de Macedo <119904806+grdlondon@users.noreply.github.com>
2023-02-25 17:51:03 -0300

    Resolvendo Problemas Código
    Estamos em andamento para resolução da linha de ´ códigos de JavaScript

--- html/index.js
+++ html/index.js
@@ -1,20 +1,20 @@
 
 function insert(num)
+
 {
-   function insert(num) 
-      var numero = document.querySelector('#resultado').innerHTML;
-      document.querySelector('#resultado').innerHTML = numero + num;
+      var numero = document.getElementById('resultado').innerHTML;
+     document.getElementById('resultado').innerHTML= numero + num;
 
 }
 
 function clean()
 {
-document.getElementsById('resultado'). innerHTML= "";
+document.getElementById('resultado').innerHTML= "";
 }
 
 function back()
 {
-var resultado = document.getElementsById('resultado').innerHTML;
+var resultado = document.getElementById('resultado').innerHTML;
 }
 
 
